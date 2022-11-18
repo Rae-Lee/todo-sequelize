41d0599492ca578731acf14fb1b2d6019bf4b1d9
Rae7771653 <nicole831220@gmail.com>
2022-11-18 11:31:18 +0800

    feat:project init

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+if(process.env.NODE_ENV !== 'production'){
+  const dotenv = require('dotenv').config()
+}
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+const session = require('express-session')
+
+const app = express()
+const port = process.env.port
+
+app.engine('handlebars', exphbs.engine({'defaultLayout': 'main'}))
+app.set('view engine', 'handlebars')
+app.use(express.urlencoded({extended: true}))
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+app.use(methodOverride('_method'))
+app.listen(port, (req, res) => {console.log('It is running on http://localhost:3000')})
