3819cefd6f544f8369555509477edf44f0ddf9e1
Rae7771653 <nicole831220@gmail.com>
2022-11-18 12:04:46 +0800

    feat: add login, register route and pages

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ if(process.env.NODE_ENV !== 'production'){
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const session = require('express-session')
-
+const routes = require('./routes')
 const app = express()
 const port = process.env.port
 
@@ -18,4 +18,5 @@ app.use(session({
   saveUninitialized: true
 }))
 app.use(methodOverride('_method'))
+app.use(routes)
 app.listen(port, (req, res) => {console.log('It is running on http://localhost:3000')})
